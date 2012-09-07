c3da17097b60d03a6cbac94ca74fd4fbb85e0c92
Michael Wu <mw10013@gmail.com>
2012-09-07 14:51:45 -0400

    validate: doc

--- src/remix_site/views/validate.clj
+++ src/remix_site/views/validate.clj
@@ -15,24 +15,38 @@
      ~@body
      (when (~k ~errors) [:span.help-inline (join \space (~k ~errors))])]])
 
-(defrh validate "/validate" {:keys [params flash errors] :as req}
-  (layout
-   [:div.container
-    [:div.page-header
-     [:h1 "Validate " [:small "With apologies to "
-                       (link-to "https://github.com/weavejester/valip" "valip")]]]
-    (when flash (html [:div.alert.alert-success.fade.in
+(defhtml form [params errors flash]
+  (form-to {:class :form-horizontal} [:post "/validate-postback"]
+             [:legend "Kick the Tires"]
+             (when flash (html [:div.alert.alert-success.fade.in
                        [:button.close {:type :button :data-dismiss :alert} "x"]
                        flash]))
-    (clj-snippet)
-    (form-to {:class :form-horizontal} [:post "/validate-postback"]
-             [:legend "Validate form example"]
              (control-group :name "Name" errors (text-field :name (:name params)))
              (control-group :password "Password" errors (password-field :password (:password params)))
              (control-group :confirm-password "Confirm Password" errors (password-field :confirm-password (:confirm-password params)))
              (control-group :fav-num "Favorite Number" errors (text-field :fav-num (:fav-num params)))
              [:div.form-actions
-              (submit-button {:class "btn btn-primary"} "Submit")])]))
+              (submit-button {:class "btn btn-primary"} "Submit")]))
+
+(defrh validate "/validate" {:keys [params flash errors] :as req}
+  (layout
+   [:div.container
+    [:div.page-header
+     [:h1 "Validate " [:small "with apologies to "
+                       (link-to "https://github.com/weavejester/valip" "valip")]]]
+    [:p (link-to "https://github.com/mw10013/remix/blob/master/src/remix/validate.clj" "Machinery ") "to validate a map."]
+    [:p [:code "(invalid? map & rules)"] " validates map against rules returning a map of errors or nil."]
+    [:p "Rules are vectors"
+     [:ul
+      [:li [:code "[key predicate error]"]]
+      [:li [:code "[val-fn key predicate error]"]]]
+     "The value for " [:code "key" ] " in the map to be validated is passed to " [:code "predicate"] ".
+If " [:code "predicate"] " returns falsy, " [:code "error"] " is added to the error map " [:code "{key [error]}"] ". If "
+     [:code "val-fn"] " is specified, it takes the map to be validated and the result is passed to " [:code "predicate"] "."]
+    [:p "A rule can also be a collection containing rules and the first invalid rule gets its " [:code "error"] " into the error map."]
+    (form params errors flash)
+    [:h2 (link-to "https://github.com/mw10013/remix-site/blob/master/src/remix_site/views/validate.clj" "Example")]
+    (clj-snippet)]))
 
 (defrh :post "/validate-postback" {:keys [params] :as req}
   (if-let [errors (invalid? params
@@ -40,11 +54,25 @@
                             [[:password (complement blank?) "Required."]
                              [:confirm-password (complement blank?) "Required."]
                              [identity :password #(apply = (map % [:password :confirm-password])) "Passwords must match."]]
-                            [:fav-num #(or (blank? %) (try (Double. %) (catch Exception e false))) "Invalid number."])]
+                            [:fav-num #(or (blank? %) (try (Double. %) (catch Exception _ false))) "Invalid number."])]
     (validate (assoc req :errors errors))
     (validate (assoc req :errors nil :flash "Valid."))))
 
 (defhtml clj-snippet []
-  [:pre.prettyprint.lang-clj.linenums "
+  [:pre.prettyprint.lang-clj"
 (ns remix-site.views.validate
-  (:use [remix.validate :only [invalid?]]))"])
+  (:use [remix.validate :only [invalid?]]))
+
+(defrh :post \"/validate-postback\" {:keys [params] :as req}
+  (if-let [errors (invalid? params
+                            [:name (complement blank?) \"Required.\"]
+                            [[:password (complement blank?) \"Required.\"]
+                             [:confirm-password (complement blank?) \"Required.\"]
+                             [identity :password
+                              #(apply = (map % [:password :confirm-password]))
+                              \"Passwords must match.\"]]
+                            [:fav-num #(or (blank? %)
+                                           (try (Double. %) (catch Exception _ false)))
+                             \"Invalid number.\"])]
+    (validate (assoc req :errors errors))
+    (validate (assoc req :errors nil :flash \"Valid.\"))))"])
