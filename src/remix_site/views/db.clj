b20405aa6b4ccb8a5db749245327e52df86d60f8
Michael Wu <mw10013@gmail.com>
2012-09-15 02:31:23 -0400

    db: with-db

--- src/remix_site/views/db.clj
+++ src/remix_site/views/db.clj
@@ -3,7 +3,7 @@
         [hiccup [core :only [html]] [def :only [defhtml]] [element :only [link-to]]]
         [remix-site.views.common :only [layout link-to-korma link-to-jdbc clj-snippet]]))
 
-(declare create-db-snippet)
+(declare create-db-snippet with-db-snippet)
 
 (defrh "/db" []
   (layout
@@ -29,33 +29,29 @@
          [:li [:code ":max-idle-time-excess-in-sec"]]
          [:li [:code ":max-idle-time-in-sec"]]]]
        [:li [:code ":naming-strategy"] " for " (link-to-jdbc) " and is optional."]]]
-     [:div.span7 (create-db-snippet)]]]))
+     [:div.span7 (create-db-snippet)]]
+    [:div.row
+     [:div.span5 (with-db-snippet)]
+     [:div.span7
+      [:p [:code "(with-db db &body)"] " evaluates body in the context of a new/existing connection
+to db with the db's naming strategy."]]]]))
 
 (defn- create-db-snippet []
-  (clj-snippet "
-(use 'remix.db)
-(create-db
- {:datasource-spec
-  {:classname \"org.hsqldb.jdbcDriver\"
-   :subprotocol \"hsqldb\"
-   :subname \"remix_test_hsqldb\"}
-  :pool-spec {:idle-time-excess-in-sec (* 15 60)
-              :idle-time (* 30 60)}
-  :naming-strategy {:keys #(-> % clojure.string/lower-case
-                               (clojure.string/replace \\_ \\-))
-                    :fields #(clojure.string/replace % \\- \\_)}}) "))
+  (clj-snippet "(use 'remix.db)
+(defonce db
+  (create-db
+   {:datasource-spec
+    {:classname \"org.hsqldb.jdbcDriver\"
+     :subprotocol \"hsqldb\"
+     :subname \"remix_test_hsqldb\"}
+    :pool-spec {:idle-time-excess-in-sec (* 15 60)
+                :idle-time (* 30 60)}
+    :naming-strategy
+    {:keys #(-> % clojure.string/lower-case
+                (clojure.string/replace \\_ \\-))
+     :fields #(clojure.string/replace % \\- \\_)}}))"))
 
-(comment
-  (defn create-db [db-spec]
-  (let [datasource-spec (:datasource-spec db-spec)
-        pool-spec (:pool-spec db-spec)
-        cpds (doto (ComboPooledDataSource.)
-               (.setDriverClass (:classname datasource-spec))
-               (.setJdbcUrl (str "jdbc:" (:subprotocol datasource-spec) ":" (:subname datasource-spec)))
-               (.setUser (:user datasource-spec))
-               (.setPassword (:password datasource-spec))
-               (.setMaxIdleTimeExcessConnections (or (:max-idle-time-excess-in-sec pool-spec)
-                                                     (* 15 60)))
-               (.setMaxIdleTime (or (:max-idle-time-in-sec pool-spec) (* 30 60))))]
-    {:connection-spec {:datasource cpds}
-     :naming-strategy (:naming-strategy db-spec)})))
+(defn- with-db-snippet []
+  (clj-snippet "(with-db db
+  (clojure.java.jdbc/drop-table :table))"))
+
