fc0f712ef5d1f4de13f14c85f0f6ee1b7e0c1a3d
Michael Wu <mw10013@gmail.com>
2012-09-27 00:31:21 -0400

    revert back to clj 1.3

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring/ring "1.1.3"]
                  [ring/ring-jetty-adapter "1.1.3"]
                  [clj-time "0.4.4"]
