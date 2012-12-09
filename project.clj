a42e162210392d00b89c0b39ff2eb5aade9a5d1d
Michael Wu <mw10013@gmail.com>
2012-12-09 00:06:20 -0500

    remix 0.0.8

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring/ring "1.1.6"]
                  [clj-time "0.4.4"]
-                 [org.clojars.mw10013/remix "0.0.6"]]
+                 [org.clojars.mw10013/remix "0.0.8"]]
   :min-lein-version "2.0.0"
   :profiles {:dev {:dependencies [[org.hsqldb/hsqldb "2.2.8"]]}})
